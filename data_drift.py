7dfe87892ce16716a9b7b453fef55005619d7db7
chaima930 <khalouaouichayma@gmail.com>
2024-04-06 12:14:09 +0200

    add modifications to data_drift.py

--- data_drift.py
+++ data_drift.py
@@ -1,43 +1,52 @@
 import pandas as pd
-import evidently
-from evidently.report import Report
+import lightgbm as lgb  # Importation de lightgbm
+from evidently.dashboard import Dashboard
 from evidently.metric_preset import DataDriftPreset
 from evidently import ColumnMapping
 import numpy as np 
 import time as time 
 
 
-df = pd.read_csv('OCDSP7/data/df.csv', sep = ",")
-application_train = df.dropna(subset=['TARGET']).drop(columns=['SK_ID_CURR','TARGET'])
-application_test = df[df['TARGET'].isna()].drop(columns=['SK_ID_CURR','TARGET'])
 
-#Pour les colonnes catégorielles on va ne prendre que les colonnes ayant que des 0 et des 1 
-categorical_columns = []
+# Charger les données en supprimant l'index
+df = pd.read_csv('df.csv', sep=",")
 
-# Parcourir chaque colonne
-for col in application_train.columns:
-    # Récupérer les valeurs uniques de la colonne
-    unique_vals = set(application_train[col].unique())
-    
-    # Vérifier si les valeurs uniques sont uniquement 0, 1, et potentiellement NaN
-    if unique_vals.issubset({0, 1, np.nan}):
-        categorical_columns.append(col)
+# Diviser les données en ensembles d'entraînement et de test
+application_train = df.dropna(subset=['TARGET'])
+application_test = df[df['TARGET'].isna()]
 
-numerical_columns = [col for col in application_train.columns if col not in categorical_columns]
+# Créer un modèle LightGBM et l'entraîner sur les données d'entraînement
+params = {
+    'learning_rate': 0.1,
+    'n_estimators': 70
+}
 
+# Créer un dataset LightGBM en utilisant une seule colonne comme étiquette
+lgb_train = lgb.Dataset(application_train.drop(columns=['TARGET']), label=application_train['TARGET'])
+
+# Entraîner le modèle LightGBM
+model = lgb.train(params, lgb_train)
+
+# Obtenir l'importance des caractéristiques
+importances = model.feature_importance(importance_type='gain')
+
+# Créer un DataFrame avec les importances des caractéristiques
+feature_importances = pd.DataFrame(importances, index=application_train.drop(columns=['TARGET']).columns, columns=['Score'])
+
+# Trier les caractéristiques par score
+feature_importances = feature_importances.sort_values(by='Score')
+
+# Sélectionner les 15 premières caractéristiques
+top_features = feature_importances.tail(15)
+
+# Filtrer les ensembles de données pour ne conserver que les 15 caractéristiques les plus importantes
+top_features_names = top_features.index.tolist()
+application_train_filtered = application_train[top_features_names]
+application_test_filtered = application_test[top_features_names]
+
+
+# Définir le temps de départ
 start_time = time.time()
-
-# Vérifiez que vos deux DataFrames ont exactement les mêmes colonnes
-assert set(application_train.columns) == set(application_test.columns)
-
-# Si l'assertion est réussie, cela signifie que les colonnes correspondent
-print("Les colonnes correspondent!")
-
-# Création du column mapping
-column_mapping = ColumnMapping()
-
-column_mapping.numerical_features = numerical_columns
-column_mapping.categorical_features = categorical_columns
 
 # Créer le rapport de dérive des données
 data_drift_report = Report(metrics=[
@@ -46,7 +55,7 @@ data_drift_report = Report(metrics=[
 
 print("Création du data_drift_report")
 
-data_drift_report.run(reference_data=application_train, current_data=application_test, column_mapping=column_mapping)
+data_drift_report.run(reference_data=application_train_filtered, current_data=application_test_filtered, column_mapping=None)
 
 print("Run du data_drift_report")
 
@@ -54,8 +63,4 @@ elapsed_time_fit = time.time() - start_time
 print(elapsed_time_fit)
 
 # Sauvegardez le rapport en tant que fichier HTML
-data_drift_report.save_html('data_drift.html')
-
-
-
-
+data_drift_report.save_html('data_drift_report.html')
